9670bbe58913d7434d60f76e4584092d86a55792
Nathália Menini <nathmenini@gmail.com>
2017-10-26 12:08:10 -0200

    corrigidos alguns bugs

--- scripts/number-bp-strips.py
+++ scripts/number-bp-strips.py
@@ -5,11 +5,11 @@ import numpy as np
 
 def numberBp(p):
 	
-	adj = 0
-	index = []
-	count_strip = 0
-	i_count = 0
+	adj = 1
+	sCresc = 0
+	sDecresc = 0
 	size_strip = []
+	bp = 0
 	
 	# introduz os elementos 0 e n+1 na permutacao
 	p = [0,] + p + [len(p)+1]
@@ -17,51 +17,75 @@ def numberBp(p):
 	# percorre a permutadao extendida procurando pelos bps
 	for i in range(0,len(p)-1):
 
-		# elementos que sao adjacentes
-		if(np.abs(p[i+1] - p[i]) == 1):
 
-			# numero de elementos totais que sao adjacentes
+		# strips crescentes
+		if(p[i+1] - p[i] == 1):
 			adj = adj + 1
 
-			# conta a quantidade de elementos na strip
-			i_count = i_count + 1
+			# Conta quantas strips crescentes tem
+			if(adj == 2):
+				sCresc = sCresc + 1
 
-			# caso seja o primeiro elemento da strip, armazena a posicao
-			if(i_count == 1):
-				index.append(i)
+			# Se for a ultima iteracao, armazena o tamanho da strip
+			if(i == len(p) - 2):
+				size_strip.append(adj)
+
+		# strips decrescentes
+		elif(p[i+1] - p[i] == -1):
+			adj = adj + 1
+
+			# Conta quantas strips decrescentes tem
+			if(adj == 2):
+				sDecresc = sDecresc + 1
+
+			# Se for a ultima iteracao, armazena o tamanho da strip
+			if(i == len(p) - 2):
+				size_strip.append(adj)
 
 		# se nao for adjacente
 		else:
-			# se tiver passado por alguma strip, contabiliza o seu tamanho
-			if(i_count != 0):
-				# armazena aonde termina a strip
-				index.append(i)
+			# incrementamos o numero de breakpoints
+			bp = bp + 1
 
-				# o tamanho final da strip
-				size_strip.append(i_count+1)
-
-				# zera o tamanho da strip, para utilizar em uma outra
-				i_count = 0
-
-		# se for a ultima iteracao e estivermos em uma strip
-		if((i == len(p) - 2) & (i_count != 0)):
-			index.append(i)
-			size_strip.append(i_count)
-
-		# conta a quantidade de breakpoints
-		bp = len(p) - 1 - adj
+			# se passou por alguma strip, armazena o tamanho dela
+			if(adj != 1):
+				size_strip.append(adj)
+				adj = 1
 	
-	return bp, np.min(size_strip), np.max(size_strip)
+	# armazena a quantidade de strips unitarias (que sao decrescentes)
+	for j in range(len(p)-np.sum(size_strip)):
+		size_strip.append(1)
+		sDecresc = sDecresc + 1
+
+	return bp, size_strip, sCresc, sDecresc
 
 
 
 # exemplos para teste
 
 #print numberBp([1, 2, 3, 5, 6, 4])
+# 0, 1, 2, 3, 5, 6, 4, 7
+# bp = 3
+# Tamanhos = 4, 2
+# strips = 4
+# Crescentes = 2
+# Decrescentes = 2
 
 #print numberBp([2, 3, 1, 5, 6, 4])
+# 0, 2, 3, 1, 5, 6, 4, 7
+# bp = 5
+# Tamanhos = 2, 2, 1, 1, 1, 1
+# strips = 6
+# Crescentes = 2
+# Decrescentes = 4
 
-#print numberBp([1,2,3,4,5,7,6,8,9,11,13,10,12])
+#print numberBp([1, 2, 3, 4, 5, 7, 6, 8, 9, 11 ,13, 10, 12])
+# 0, 1, 2, 3, 4, 5, 7, 6, 8, 9, 11 ,13, 10, 12, 13
+# bp = 7
+# Tamanhos = 6, 2, 2, 1, 1, 1, 1, 1
+# strips = 8
+# Crescentes = 2
+# Decrescentes = 6
 
 
 
