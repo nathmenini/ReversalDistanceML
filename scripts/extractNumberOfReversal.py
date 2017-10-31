d40f301b40f125e591224b61f3a9a12e383e36a8
JorgeLuiz <jre-jl@hotmail.com>
2017-10-31 09:37:38 -0200

    Melhorando a extração do número de reversões por permutação
    Melhorando o script responsável pela extração do número de reversões por permutação devido a ocorrência de alguns erros ao obter elementos da tela.

--- scripts/extractNumberOfReversal.py
+++ scripts/extractNumberOfReversal.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 import createPermutations
 import re
+import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 def execute():
 
@@ -10,34 +15,43 @@ def execute():
     assert "Bioinfo" in driver.title #certifica que esta na pagina correta
     assert "No results found." not in driver.page_source #valida acesso a pagina
     
-    #s_perms = [[[3, 1, 4, 5, 2],[5, 2, 1, 4, 3]],[[3, 1, 4, 5, 2, 6],[6, 5, 2, 1, 4, 3]], [[8, 5, 1, 4, 3, 2, 7, 6]]]
-    s_perms = createPermutations.makePermutations()
+    perm, s_perms = createPermutations.makePermutations()
     
     #HashMap com o resultado no formato -> [permutacao(key), numero de reversoes(valor)]
     resultHash = {}
     
     for i in s_perms:
         for j in i:
-            for k in j:
+                
+                time.sleep(0.6)
                 
                 #Configura permutacao
-                permutacao = ' '.join(str(e) for e in k)
-                
+                permutacao = ' '.join(str(e) for e in j)
+
                 #Obtem referencia ao campo texto para entrada da permutacao
-                elem_text_field = driver.find_element_by_name("search_distance_form:permutation_input")
+                elem_text_field = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.NAME, "search_distance_form:permutation_input")))
+                #elem_text_field = driver.find_element_by_name("search_distance_form:permutation_input")
                 #Limpa o campo para nova entrada
                 elem_text_field.clear()
                 #Insere a nova permutacao obtida de s_perms
                 elem_text_field.send_keys(permutacao)
                 
-                #Efetua click no botao 'search' da pagina
-                elem_search_button = driver.find_element_by_id("search_distance_form:j_id38").click()
+                time.sleep(0.6)
                 
+                #Efetua click no botao 'search' da pagina
+                elem_search_button = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, "search_distance_form:j_id38")))
+                elem_search_button.click()
+                #elem_search_button = driver.find_element_by_id("search_distance_form:j_id38").click()
+                
+                time.sleep(0.6)
+                #driver.implicitly_wait(10) # seconds 
+                #driver.refresh();
                 #Obtem referencia ao elemento da pagina que contem o texto com o numero de reversoes para a permutacao
-                elem_search_button = driver.find_element_by_class_name("generalStyle")
+                elem_result = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CLASS_NAME, "generalStyle")))
+                #elem_search_button = driver.find_element_by_class_name("generalStyle")
                 
                 #Faz um parser do texto para obter o numero de reversoes
-                value = re.search("is (\d+)", elem_search_button.text, re.IGNORECASE).group(1);
+                value = re.search("is (\d+)", elem_result.text, re.IGNORECASE).group(1);
                 
                 #salva no hash resultante
                 resultHash[permutacao] = value
