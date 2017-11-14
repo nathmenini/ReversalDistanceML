9d3e9d9890029dd54ead4090c206977ff76de5c0
Nathália Menini <Menini@Nathalias-MacBook-Pro.local>
2017-11-14 14:32:18 -0200

    separando em perms e dist

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -5,10 +5,12 @@ import progressbar
 fileNames = ['perm_5.txt', 'perm_6.txt', 'perm_7.txt', 'perm_8.txt', 'perm_9.txt', 'perm_10.txt']
 
 perms = []
+dist = []
 
 bar = progressbar.ProgressBar()
 for i in bar(range(len(fileNames))):
 	p = np.loadtxt(fileNames[i], delimiter = " ")
-	perms.append(p)
-
+	perms.append(p[:,:-1])
+	dist = np.hstack((dist, p[:,p.shape[1]-1]))
+	
 # Executar o getInputs para o perms
