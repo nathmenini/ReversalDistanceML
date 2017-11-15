ab4f3727f13527a45f2aab1fe1abeda7f9f472d3
Sergio <serza.arnosti@gmail.com>
2017-11-15 12:24:51 -0200

    algoritmo ingenuo de reversao

--- scripts/simpleReversalSort.py
+++ scripts/simpleReversalSort.py
@@ -4,11 +4,14 @@ import progressbar
 
 def SimpleReversalSort(pi):
 
+	count=0
+
 	bar = progressbar.ProgressBar()
-    for i in bar(xrange(len(pi))):
-    	j=pi.index(min(pi[i:])) 
+	for i in bar(range(len(pi))):
+		j=pi.index(min(pi[i:]))
 		if(j != i):
 			pi = pi[:i] + [v for v in reversed(pi[i:j+1])] + pi[j+1:]
-			print “rho(%d,%d) = %s” % (i, j, pi)
-			
-	return pi 
+			count=count+1
+			#print("rho(%d,%d) = %s" % (i, j, pi))
+
+	return count 
