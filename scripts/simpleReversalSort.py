e7308959aaba7734a38ea00395301882981f25c7
Nathália Menini <Menini@Nathalias-MacBook-Pro.local>
2017-11-15 16:16:03 -0200

    Revert "sort"
    This reverts commit ba41f45007de58641529da404a914485060ba8bc.

---
+++ scripts/simpleReversalSort.py
@@ -0,0 +1,14 @@
+import networkx as nx
+import numpy as np
+import progressbar
+
+def SimpleReversalSort(pi):
+
+	bar = progressbar.ProgressBar()
+    for i in bar(xrange(len(pi))):
+    	j=pi.index(min(pi[i:])) 
+		if(j != i):
+			pi = pi[:i] + [v for v in reversed(pi[i:j+1])] + pi[j+1:]
+			print “rho(%d,%d) = %s” % (i, j, pi)
+			
+	return pi 
