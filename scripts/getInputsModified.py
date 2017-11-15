158f583f8c5ccf028bfe9ab9558907ef5622b0d0
Sergio <serza.arnosti@gmail.com>
2017-11-15 15:14:57 -0200

    algoritmo ingenuo e getInputs para ele

---
+++ scripts/getInputsModified.py
@@ -0,0 +1,29 @@
+import numpy as np
+import progressbar
+from simpleReversalSort import SimpleReversalSort
+
+# Modificado para calcular para outros algoritmos
+# 1- SimpleReversalSort - algoritmo ingenuo de reversao
+
+# retorno [numero de reversoes, tamanho da permutação]
+
+def getInputsModified(p):
+	# vetor para armazenar as caracteristicas
+	features = np.zeros((1,1))
+	
+	# vetor para identificar o tamanho de cada permutacao
+	labels = np.zeros((1,1))
+	lengths = range(5,11)
+	
+	bar = progressbar.ProgressBar()
+	for i in bar(range(len(p))):
+		pi = p[i]
+		features = np.vstack((features, np.apply_along_axis(SimpleReversalSort, 1, pi))) # linhas da matriz com as features
+		labels = np.vstack((labels, np.reshape(np.repeat(lengths[i], pi.shape[0]),(pi.shape[0],1)))) # linhas com o tamanho da permutacao
+	
+	features = np.delete(features, 0, 0)
+	labels = np.delete(labels, 0, 0)
+
+	features = np.hstack((features, labels)) # junta as colunas
+
+	return features
--- scripts/simpleReversalSort.py
+++ scripts/simpleReversalSort.py
@@ -5,13 +5,13 @@ import progressbar
 def SimpleReversalSort(pi):
 
 	count=0
+	pi = pi.tolist()
 
-	bar = progressbar.ProgressBar()
-	for i in bar(range(len(pi))):
+	for i in range(len(pi)):
 		j=pi.index(min(pi[i:]))
 		if(j != i):
 			pi = pi[:i] + [v for v in reversed(pi[i:j+1])] + pi[j+1:]
 			count=count+1
 			#print("rho(%d,%d) = %s" % (i, j, pi))
 
-	return count 
+	return count,
