a2ad80d3cd618a9f9a4cb22e6aef9f1e55b8a4e2
Sergio <serza.arnosti@gmail.com>
2017-11-15 16:20:41 -0200

    update

--- scripts/simpleReversalSort.py
+++ scripts/simpleReversalSort.py
@@ -2,7 +2,6 @@ import networkx as nx
 import numpy as np
 import progressbar
 
-<<<<<<< HEAD
 # algoritmo ingenuo
 
 def SimpleReversalSort(pi):
@@ -18,15 +17,4 @@ def SimpleReversalSort(pi):
 			#print("rho(%d,%d) = %s" % (i, j, pi))
 
 	return count,
-=======
-def SimpleReversalSort(pi):
 
-	bar = progressbar.ProgressBar()
-    for i in bar(xrange(len(pi))):
-    	j=pi.index(min(pi[i:])) 
-		if(j != i):
-			pi = pi[:i] + [v for v in reversed(pi[i:j+1])] + pi[j+1:]
-			print “rho(%d,%d) = %s” % (i, j, pi)
-			
-	return pi 
->>>>>>> e7308959aaba7734a38ea00395301882981f25c7
